813e4a0862de4a652d60c75efeca6056a756eade
sazyansari <samiransari96672@gmail.com>
2021-05-20 23:08:25 +0530

    all added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,54 @@
   const glightbox = GLightbox({
     selector: ".glightbox",
   });
+
+  new Swiper(".gallery-slider", {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  const galleryLightbox = GLightbox({
+    selector: ".gallery-lightbox",
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
 })();
